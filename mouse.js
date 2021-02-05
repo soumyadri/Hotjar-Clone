965a235340f532bcd0a6ffcf1767cfc6324826ca
khushbookhator <khushbookhator11@gmail.com>
2021-02-05 22:37:43 +0530

    new

--- mouse.js
+++ mouse.js
@@ -1,10 +1,119 @@
 var imgs = document.getElementById("img1");
-var pics = document.getElementById("pict")
 function newImage() {
     imgs.src="new.jpeg"
 }
 function oldImage() {
     imgs.src="https://static-cms.hotjar.com/images/what-is-CX.width-750.png"
 }
-imgs.addEventListener('mouseover', mouseIn);
-imgs.addEventListener('mouseout', mouseOut);
+imgs.addEventListener('mouseover', newImage);
+imgs.addEventListener('mouseout', oldImage);
+
+var imgs2 = document.getElementById("img2");
+function newImage2() {
+    imgs2.src="second.jpeg"
+}
+function oldImage2() {
+    imgs2.src="https://static-cms.hotjar.com/images/session-recording-example-1.width-750.jpg"
+}
+imgs2.addEventListener('mouseover', newImage2);
+imgs2.addEventListener('mouseout', oldImage2);
+
+var imgs3 = document.getElementById("img3");
+function newImage3() {
+    imgs3.src="3.jpeg"
+}
+function oldImage3() {
+    imgs3.src="https://static-cms.hotjar.com/documents/what-is-net-promoter-score_USNvPZC.svg"
+}
+imgs3.addEventListener('mouseover', newImage3);
+imgs3.addEventListener('mouseout', oldImage3);
+
+var imgs4 = document.getElementById("img4");
+function newImage4() {
+    imgs4.src="4.jpeg"
+}
+function oldImage4() {
+    imgs4.src="https://static-cms.hotjar.com/documents/conducting-usability-test_yon4BQT.svg"
+}
+imgs4.addEventListener('mouseover', newImage4);
+imgs4.addEventListener('mouseout', oldImage4);
+
+var imgs5 = document.getElementById("img5");
+function newImage5() {
+    imgs5.src="5.jpeg"
+}
+function oldImage5() {
+    imgs5.src="https://static-cms.hotjar.com/documents/Conversion-rate-optimization_ZCfvG32.svg"
+}
+imgs5.addEventListener('mouseover', newImage5);
+imgs5.addEventListener('mouseout', oldImage5);
+
+var imgs6 = document.getElementById("img6");
+function newImage6() {
+    imgs6.src="6.jpeg"
+}
+function oldImage6() {
+    imgs6.src="https://static-cms.hotjar.com/documents/Customer-journey-mapping.svg"
+}
+imgs6.addEventListener('mouseover', newImage6);
+imgs6.addEventListener('mouseout', oldImage6);
+
+var imgs7 = document.getElementById("img7");
+function newImage7() {
+    imgs7.src="7.jpeg"
+}
+function oldImage7() {
+    imgs7.src="https://static-cms.hotjar.com/documents/Customer-journey-mapping.svg"
+}
+imgs7.addEventListener('mouseover', newImage7);
+imgs7.addEventListener('mouseout', oldImage7);
+
+var imgs8 = document.getElementById("img8");
+function newImage8() {
+    imgs8.src="8.jpeg"
+}
+function oldImage8() {
+    imgs8.src="https://static-cms.hotjar.com/documents/CRO-program_hVKL0e4.svg"
+}
+imgs8.addEventListener('mouseover', newImage8);
+imgs8.addEventListener('mouseout', oldImage8);
+
+var imgs9 = document.getElementById("img9");
+function newImage9() {
+    imgs9.src="9.jpeg"
+}
+function oldImage9() {
+    imgs9.src="https://static-cms.hotjar.com/documents/analyze-evaluate-usability-test_Cy90nht.svg"
+}
+imgs9.addEventListener('mouseover', newImage9);
+imgs9.addEventListener('mouseout', oldImage9);
+
+var imgs10 = document.getElementById("img10");
+function newImage10() {
+    imgs10.src="10.jpeg"
+}
+function oldImage10() {
+    imgs10.src="https://static-cms.hotjar.com/images/eye.width-750.png"
+}
+imgs10.addEventListener('mouseover', newImage10);
+imgs10.addEventListener('mouseout', oldImage10);
+
+var imgs11 = document.getElementById("img11");
+function newImage11() {
+    imgs11.src="11.jpeg"
+}
+function oldImage11() {
+    imgs11.src="https://static-cms.hotjar.com/images/lightning-talks.width-750.png"
+}
+imgs11.addEventListener('mouseover', newImage11);
+imgs11.addEventListener('mouseout', oldImage11);
+
+var imgs12 = document.getElementById("img12");
+function newImage12() {
+    imgs12.src="12.jpeg"
+}
+function oldImage12() {
+    imgs12.src="https://static-cms.hotjar.com/documents/image4.svg"
+}
+imgs12.addEventListener('mouseover', newImage12);
+imgs12.addEventListener('mouseout', oldImage12);
