f87c571cab97550f47b8d484d3f80dce166f37fa
khushbookhator <khushbookhator11@gmail.com>
2021-02-05 10:49:21 +0530

    functions removed

--- buttons.js
+++ buttons.js
@@ -1,32 +1,94 @@
 
-function moreP() {
-    var divs = document.getElementById("full")
-    var btn0 = document.getElementById("btn0")
-    var newpara = document.getElementById("new")
-    var newButton = document.createElement("button")
-    newButton.addEventListener('click', lessP)
-    btn0.remove();
-    divs.style.background = "lavender";
-    divs.style.padding="2%"
-    newpara.textContent = "Nobody wants to waste their time on a website that is confusing and hard to navigate.You know it, we know it, and website owners know it too—which is why they turn to Hotjar. Website owners use Hotjar to better understand user behavior: what actions people take on a website, what elements of a page they click or ignore, what they enjoy, where they struggle. With this insight, website owners can then make changes that improve the overall experience for all website users. And that includes you."
-    newButton.innerHTML = "Show Less"
-    newButton.style.backgroundColor = "lavender"
-    newButton.style.width = "80px"
-    divs.appendChild(newButton)
+// function moreP() {
+//     var divs = document.getElementById("full")
+//     var btn0 = document.getElementById("btn0")
+//     var newpara = document.getElementById("new")
+//     var newButton = document.createElement("button")
+//     newButton.addEventListener('click', lessP)
+//     btn0.remove();
+//     divs.style.background = "lavender";
+//     divs.style.padding="2%"
+//     newpara.textContent = "Nobody wants to waste their time on a website that is confusing and hard to navigate.You know it, we know it, and website owners know it too—which is why they turn to Hotjar. Website owners use Hotjar to better understand user behavior: what actions people take on a website, what elements of a page they click or ignore, what they enjoy, where they struggle. With this insight, website owners can then make changes that improve the overall experience for all website users. And that includes you."
+//     newButton.innerHTML = "Show Less"
+//     newButton.style.backgroundColor = "lavender"
+//     newButton.style.width = "80px"
+//     divs.appendChild(newButton)
     
-}
+// }
 
-function lessP() {
-    var divs = document.getElementById("full")
-    divs.innerHTML = "";
-    divs.style.background = "white"
-    divs.style.paddingTop = "8%"
-    divs.style.paddingLeft = "5%"
-    divs.style.paddingRight = "3%"
-    var h1 = document.createElement("h1")
-    var p = document.createElement("p")
-    h1.textContent = "Improving websites, for you."
-    divs.appendChild(h1)
-    p.textContent = "Online businesses use Hotjar to create better website experiences."
-    divs.appendChild(p)
-}
+// function lessP() {
+//     var divs = document.getElementById("full")
+//     divs.innerHTML = "";
+//     divs.style.background = "white"
+//     divs.style.paddingTop = "8%"
+//     divs.style.paddingLeft = "5%"
+//     divs.style.paddingRight = "3%"
+//     var h1 = document.createElement("h1")
+//     var p = document.createElement("p")
+//     h1.textContent = "Improving websites, for you."
+//     divs.appendChild(h1)
+//     p.textContent = "Online businesses use Hotjar to create better website experiences."
+//     divs.appendChild(p)
+// }
+
+// function morePOne() {
+//     var divs_a = document.getElementById("fullOne")
+//     var btn_a = document.getElementById("btnOne")
+//     var newpara_a = document.getElementById("newOne")
+//     var newButton_a = document.createElement("button")
+//     newButton_a.addEventListener('click', showLess)
+//     btn_a.remove();
+//     divs_a.style.background = "lavender";
+//     divs_a.style.padding="2%"
+//     newpara_a.textContent = "When you browse a website that has Hotjar installed, we collect data about your interactions (e.g. where you click, scroll your mouse, move in between pages) and use it to create visual representations of the experience. Website owners get these insights without tracking IP/MAC addresses-- the stuff that relates to the device you use."
+//     newButton_a.innerHTML = "Show Less"
+//     newButton_a.style.backgroundColor = "lavender"
+//     newButton_a.style.width = "80px"
+//     divs_a.appendChild(newButton_a)
+    
+// }
+//     function showLess() {
+//     var divs_a = document.getElementById("fullOne")
+//     divs_a.innerHTML = "";
+//     divs_a.style.background = "white"
+//     divs_a.style.paddingTop = "8%"
+//     divs_a.style.paddingLeft = "5%"
+//     divs_a.style.paddingRight = "3%"
+//     var h1 = document.createElement("h1")
+//     var p = document.createElement("p")
+//     h1.textContent = "We record interactions, not your sensitive data."
+//     divs_a.appendChild(h1)
+//     p.textContent = "Hotjar is designed for websites to get insights. And respect your privacy."
+//     divs_a.appendChild(p)
+// }
+
+// function morePTwo() {
+//     var divs_a = document.getElementById("fullTwo")
+//     var btn_a = document.getElementById("btnTwo")
+//     var newpara_a = document.getElementById("newTwo")
+//     var newButton_a = document.createElement("button")
+//     newButton_a.addEventListener('click', show2Less)
+//     btn_a.remove();
+//     divs_a.style.background = "lavender";
+//     divs_a.style.padding="2%"
+//     newpara_a.textContent = "When you browse a website that has Hotjar installed, we collect data about your interactions (e.g. where you click, scroll your mouse, move in between pages) and use it to create visual representations of the experience. Website owners get these insights without tracking IP/MAC addresses-- the stuff that relates to the device you use."
+//     newButton_a.innerHTML = "Show Less"
+//     newButton_a.style.backgroundColor = "lavender"
+//     newButton_a.style.width = "80px"
+//     divs_a.appendChild(newButton_a)
+    
+// }
+//     function show2Less() {
+//     var divs_a = document.getElementById("fullTwo")
+//     divs_a.innerHTML = "";
+//     divs_a.style.background = "white"
+//     divs_a.style.paddingTop = "8%"
+//     divs_a.style.paddingLeft = "5%"
+//     divs_a.style.paddingRight = "3%"
+//     var h1 = document.createElement("h1")
+//     var p = document.createElement("p")
+//     h1.textContent = "Improving websites, for you."
+//     divs_a.appendChild(h1)
+//     p.textContent = "Online businesses use Hotjar to create better website experiences."
+//     divs_a.appendChild(p)
+// }
